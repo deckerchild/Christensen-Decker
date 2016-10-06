6c8906810f661c7a3822cb57b7d0071fd8ea0d2f
mount <mount@10.38.165.215>
2016-10-06 09:06:45 -0600

    Commit to Repository 10/4/2016 - Mark is trying

---
+++ StrangerThings/src/byui/cit260/strangerthings/model/Player.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.strangerthings.model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author mount
+ */
+public class Player implements Serializable{
+    
+    private String name;
+    private String attribute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    
+    
+}
