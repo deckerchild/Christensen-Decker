faf63c8a8b179baf4b652843c818118b7b6f4866
mount <mount@10.38.165.215>
2016-10-07 09:57:47 -0600

    Commit to Repository 10/4/2016 - Mark's first two classes are done.

---
+++ StrangerThings/src/byui/cit260/strangerthings/model/Monster.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.strangerthings.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mount
+ */
+public class Monster implements Serializable {
+    private String name;
+    private int strength;
+
+    public Monster() {
+    }
+   
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + this.strength;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Monster other = (Monster) obj;
+        if (this.strength != other.strength) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" + "name=" + name + ", strength=" + strength + '}';
+    }
+    
+    
+    
+}
