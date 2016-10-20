500e6e4d1bcd1a45081a3c4d784179d25148486d
mount <mount@10.38.165.215>
2016-10-20 09:53:13 -0600

    Merge origin/master - Test for Navigation
    Conflicts:
    	StrangerThings/src/strangerthings/StrangerThings.java

--- StrangerThings/src/buyi/cit460/curiousworkmanship/control/Navigation.java
+++ StrangerThings/src/buyi/cit460/curiousworkmanship/control/Navigation.java
@@ -5,10 +5,32 @@
  */
 package buyi.cit460.curiousworkmanship.control;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author tcfat
  */
 public class Navigation {
     
+    public int shackCalculation(int x, int i, int answer){
+        int result;
+        if(i<=0 || x<=0)
+            result = -1;
+        else if((((i/2)+x)/3) == answer)
+                result = 1;
+        else 
+            result = 0;
+        return result;
+                             
+        }
+
+
+//Else 
+//Result = 0
+//Return result
+
 }
+    
+
---
+++ StrangerThings/test/buyi/cit460/curiousworkmanship/control/NavigationTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyi.cit460.curiousworkmanship.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mount
+ */
+public class NavigationTest {
+    
+    public NavigationTest() {
+    }
+
+    /**
+     * Test of shackCalculation method, of class Navigation.
+     */
+    @Test
+    public void testShackCalculation() {
+        System.out.println("Test Case #1");
+        int x = 12;
+        int i = 24;
+        int answer = 8;
+        Navigation instance = new Navigation();
+        int expResult = 1;
+        int result = instance.shackCalculation(x, i, answer);
+        assertEquals(expResult, result);
+    
+        System.out.println("Test Case #2");
+        x = 12;
+        i = -1;
+        answer = 3;
+        instance = new Navigation();
+        expResult = -1;
+        result = instance.shackCalculation(x, i, answer);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #3");
+        x = -1;
+        i = 24;
+        answer = 3;
+        instance = new Navigation();
+        expResult = -1;
+        result = instance.shackCalculation(x, i, answer);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #4");
+        x = 0;
+        i = 0;
+        answer = 0;
+        instance = new Navigation();
+        expResult = -1;
+        result = instance.shackCalculation(x, i, answer);
+        assertEquals(expResult, result);
+        
+    }
+    
+}
