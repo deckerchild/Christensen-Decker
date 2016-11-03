a7286a139989bccbd340a13ad05600387620c856
mount <mount@10.38.165.215>
2016-11-03 09:30:12 -0600

    Merge origin/master - Test for Navigation
    Conflicts:
    	StrangerThings/src/strangerthings/StrangerThings.java

--- StrangerThings/test/byui/cit260/strangerThings/control/NavigationTest.java
+++ StrangerThings/test/byui/cit260/strangerThings/control/NavigationTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package buyi.cit460.strangerThings.control;
+package byui.cit260.strangerThings.control;
 
 import byui.cit260.strangerThings.control.Navigation;
 import org.junit.Test;
