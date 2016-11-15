7694c8bfdf9ee5d8bd44fef0b9904fb4243cda2f
tcfat <tcfat@DESKTOP-4HV3B82>
2016-11-15 09:45:29 -0700

    11/14 set up super class

--- StrangerThings/src/byui/cit260/strangerthings/model/Character.java
+++ StrangerThings/src/byui/cit260/strangerthings/model/Character.java
@@ -10,72 +10,33 @@ import java.util.Objects;
  *
  * @author mount
  */
-public class Character implements Serializable {
-    private String name;
-    private int corrdinates;
-    private int power;
+public enum Character implements Serializable {
+    
+    Mike,
+    Lucas,
+    Dustin,
+    Eleven;
+    
+    private final int corrdinates;
+    private final int power;
 
-    public Character() {
+     public Character() {
+    }
+    Character(int power){
+        this.power = power; 
     }
     
-    
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getCorrdinates() {
         return corrdinates;
     }
 
-    public void setCorrdinates(int corrdinates) {
-        this.corrdinates = corrdinates;
-    }
 
     public int getPower() {
         return power;
     }
 
-    public void setPower(int power) {
-        this.power = power;
-    }
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 31 * hash + Objects.hashCode(this.name);
-        hash = 31 * hash + this.corrdinates;
-        hash = 31 * hash + this.power;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Character other = (Character) obj;
-        if (this.corrdinates != other.corrdinates) {
-            return false;
-        }
-        if (this.power != other.power) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
-    }
 
     @Override
     public String toString() {
