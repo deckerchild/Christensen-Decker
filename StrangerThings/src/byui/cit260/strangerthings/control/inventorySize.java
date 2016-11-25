53fc02dc218e0cd190652809010651f5240b867e
mount <mount@10.38.165.215>
2016-11-25 09:37:51 -0700

    Merge origin/master - Team Assignment 9
    Conflicts:
    	StrangerThings/src/strangerthings/StrangerThings.java

---
+++ StrangerThings/src/byui/cit260/strangerthings/control/inventorySize.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.strangerthings.control;
+
+import byui.cit260.strangerthings.model.Inventory;
+
+/**
+ *
+ * @author mount
+ */
+public class inventorySize {
+    
+    Inventory inventory = new Inventory();
+    Strangerthings.getInventoryList(Inventory);
+}
